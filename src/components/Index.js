6d81102f401e51c22aaa533984e9cc354b2febd3
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-11-12 09:29:11 +0100

    refactor: combined components

---
+++ src/components/Index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Hero from './Hero';
+import SocialProof from './SocialProof';
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <SocialProof />
+    </>
+  );
+};
+
+export default Index;
