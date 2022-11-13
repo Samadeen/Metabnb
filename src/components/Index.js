3d08d046db128717298806a4a6e98884342a8324
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-11-13 23:29:46 +0100

    feat: Added Modal

--- src/components/Index.js
+++ src/components/Index.js
@@ -3,6 +3,7 @@ import Footer from './Footer';
 import Header from './Header';
 import Hero from './Hero';
 import Meta from './Meta';
+import Modal from './Modal';
 import Place from './Place';
 import SocialProof from './SocialProof';
 
